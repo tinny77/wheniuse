d27420c1f1e9d74f60fded338eb09d4510eb24db
tinny77 <tinny77@gmail.com>
2023-10-05 10:10:12 +0200

    removed unused

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
 
 function Footer() {
 	return (
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+
 
 function Header() {
 	return (
